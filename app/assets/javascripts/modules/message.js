5d4134322704da2d656e1d0e5a2400511cbec746
KojiK222 <68413995+KojiK222@users.noreply.github.com>
2020-09-13 17:35:53 +0900

    automatic updating in chat-space

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="messagespace">
+        `<div class="messagespace" data-message-id=${message.id}>
           <div class="message-info">
             <div class="message-info__username">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     }else {
       let html =
-      `<div class="messagespace">
+      `<div class="messagespace" data-message-id=${message.id}>
         <div class="message-info">
           <div class="message-info__username">
             ${message.user_name}
